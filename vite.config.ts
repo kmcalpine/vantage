7567c00def9135a7df02203a9f3613d3ecf62cfd
Kurtis <kurtismcalpine@gmail.com>
2025-12-24 13:09:28 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/vantage/",
+    base: "/",
     plugins: [tailwindcss(), react()],
 });
