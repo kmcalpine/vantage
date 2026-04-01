2ba68c9a7ca57ea0ac86af247d7e32886b94d387
Kurtis McAlpine <kurtismcalpine@gmail.com>
2026-04-01 16:59:35 +0100

    logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,9 @@ import logo from "../assets/logo-white-icon.png";
 const Logo = () => {
     return (
         <div className="absolute left-4 h-[70px] z-[200] flex items-center">
-            <img src={logo} className="h-6"></img>
+            <a href="/">
+                <img src={logo} className="h-6"></img>
+            </a>
         </div>
     );
 };
