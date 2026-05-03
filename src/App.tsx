685f2cce39c1e8f79d3b5a2590fc56bb34d858a5
Kurtis McAlpine <kurtismcalpine@gmail.com>
2026-05-03 23:46:50 +0100

    remove labour

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SelectionPage from './SelectionPage';
-import Safety from './Safety';
-import LabourHire from './LabourHire';
-import ScrollToTop from './components/ScrollToTop';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SelectionPage from "./SelectionPage";
+import Safety from "./Safety";
+import LabourHire from "./LabourHire";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  return (
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<SelectionPage />} />
-        <Route path="/safety" element={<Safety />} />
-        <Route path="/labour-hire" element={<LabourHire />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Safety />} />
+                {/*         <Route path="/safety" element={<Safety />} />
+        <Route path="/labour-hire" element={<LabourHire />} /> */}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
